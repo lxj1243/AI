0ebb4e92f735071d0a7b82aea22326bb891587aa
bleedlord <296796098@qq.com>
2019-08-21 15:24:46 +0800

    no message

--- classification/learning/linearRegressor.py
+++ classification/learning/linearRegressor.py
@@ -9,34 +9,34 @@ from matplotlib import pyplot
 from sklearn import metrics
 import numpy as numpy
 import pandas as pandas
-import tensorflow as tensorflow
+import tensorflow as tf
 
 from tensorflow.python.data import Dataset
 
-tensorflow.logging.set_verbosity(tensorflow.logging.ERROR)
+tf.logging.set_verbosity(tf.logging.ERROR)
 pandas.options.display.max_rows = 10;
 pandas.options.display.float_format = '{:.1f}'.format
 
-california_housing_dataframe = pandas.read_csv(
+california_housing_dataFrame = pandas.read_csv(
     "https://download.mlcc.google.cn/mledu-datasets/california_housing_train.csv", sep=",")
-california_housing_dataframe = california_housing_dataframe.reindex(
-    numpy.random.permutation(california_housing_dataframe.index))
-california_housing_dataframe["median_house_value"] /= 1000.0
+california_housing_dataFrame = california_housing_dataFrame.reindex(
+    numpy.random.permutation(california_housing_dataFrame.index))
+california_housing_dataFrame["median_house_value"] /= 1000.0
 
 # 查看数据
-# california_housing_dataframe
-# california_housing_dataframe.describe()
+# california_housing_dataFrame
+# california_housing_dataFrame.describe()
 
 # 定义输入特征
-my_feature = california_housing_dataframe[["total_rooms"]]
+my_feature = california_housing_dataFrame[["total_rooms"]]
 # 为total_rooms配置一个数值特征列
-feature_columns = [tensorflow.feature_column.numeric_column("total_rooms")]
+feature_columns = [tf.feature_column.numeric_column("total_rooms")]
 # 定义标签
-targets = california_housing_dataframe["median_house_value"]
+targets = california_housing_dataFrame["median_house_value"]
 
 # 选择参数优化算法
 # GradientDescent 梯度下降
-my_optimizer = tensorflow.train.GradientDescentOptimizer(learning_rate=0.0000001)
-my_optimizer = tensorflow.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0000001)
+my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
 
-leaner_regressor = tensorflow.estimator.LinearRegressor(feature_columns=feature_columns, optimizer=my_optimizer)
+leaner_regressor = tf.estimator.LinearRegressor(feature_columns=feature_columns, optimizer=my_optimizer)
